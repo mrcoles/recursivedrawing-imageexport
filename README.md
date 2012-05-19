159a2aa7490bde63d19572500c5f90c5dd14b398
Peter Coles <peter@mrcoles.com>
2012-05-19 12:30:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Recursive Drawing Image Export
+==============================
+
+Drag this user script onto Chrome and when you go to [recursivedrawing.com](http://recursivedrawing.com/draw.html), you’ll now have an “export” link in the bottom right that will let you export whatever you have drawn as an image.
---
+++ recursivedrawing-imageexport.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name RecursiveDrawing Export
+// @namespace http://mrcoles.com
+// @description Add an export to image feature to recursivedrawing.com
+// @version 0.0.1
+// @match http://recursivedrawing.com/draw.html
+// ==/UserScript==
+
+var d = document,
+    $d = function(x) { return document.getElementById(x); },
+    sideBar = $d('sidebarRight'),
+    link = d.createElement('a'),
+    style = d.createElement('style');
+
+function exportImage() {
+    var c1 = $d('workspaceCanvas'),
+        c2 = $d('drawFurther'),
+        W = c1.width,
+        H = c2.height,
+        c3 = d.createElement('canvas'),
+        ctx = c3.getContext('2d');
+    c3.width=W;
+    c3.height=H;
+    ctx.drawImage(c1, 0, 0);
+    ctx.drawImage(c2, 0, 0);
+    window.open(c3.toDataURL());
+    return false;
+};
+
+style.innerHTML = ('a.export {' +
+                   '  position: absolute;' +
+                   '  bottom: 0;' +
+                   '  right: 0;' +
+                   '  color: #999;' +
+                   '}'
+                  );
+
+link.className = 'export';
+link.href = '#';
+link.innerHTML = 'export';
+link.onclick = exportImage;
+
+document.body.appendChild(style);
+sideBar.appendChild(link);
